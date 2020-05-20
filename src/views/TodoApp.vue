cade7a952c2216fe1386cbd7a3720caed0bd836b
john morris <fennymorris@live.co.uk>
2020-05-20 21:04:49 +0100

    add todo form

--- src/views/TodoApp.vue
+++ src/views/TodoApp.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="container">
+    <add-todo-vue />
+    <h1>Todos</h1>
     <todo-vue />
   </div>
 </template>
 
 <script>
 import TodoVue from '../components/Todo.vue'
+import AddTodoVue from '../components/AddTodo.vue'
+
 export default {
   components: {
-    TodoVue
+    TodoVue,
+    AddTodoVue
   }
 }
 </script>
