f0ae27ee6c3e1257393b29bcc9b5e260024d652e
john morris <fennymorris@live.co.uk>
2020-05-21 14:11:35 +0100

    datATABLE

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,61 @@
 <template>
-  <div class="home">
-  </div>
+  <v-app id="inspire">
+    <v-card>
+      <v-card-title>
+        Todos
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        dense
+        :headers="headers"
+        :items="allTodos"
+        :items-per-page="5"
+        class="elevation-1"
+        :search="search"
+      ></v-data-table>
+    </v-card>
+  </v-app>
 </template>
-
 <script>
-// @ is an alias to /src
+import { mapActions, mapGetters } from 'vuex'
 export default {
+  methods: {
+    ...mapActions(['fetchTodos'])
+  },
+  data () {
+    return {
+      search: '',
+      todos: [
+        // {
+        //   title: 'a',
+        //   completed: false
+        // },
+        // {
+        //   title: 'b',
+        //   completed: false
+        // }
+      ],
+      headers: [
+        { text: 'Title', value: 'title' },
+        { text: 'Completed', value: 'completed' }
+      ]
+    }
+  },
   name: 'Home',
-  components: {
+  components: {},
+  computed: {
+    ...mapGetters(['allTodos'])
+  },
+  async created () {
+    await this.fetchTodos()
+    this.loading = false
   }
 }
 </script>
