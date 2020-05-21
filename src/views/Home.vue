ca897e54e72327f1c288097de14b9c074cc9558a
john morris <fennymorris@live.co.uk>
2020-05-21 20:49:13 +0100

    ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,18 +24,57 @@
         show-expand
       >
         <template v-slot:item.completed="{ item }">
-          <v-chip :color="getColor(item.completed)" dark>{{ item.completed }}</v-chip>
+          <v-chip :color="getColor(item.completed)" dark>{{ item.completed ? 'YES': 'NO' }}</v-chip>
         </template>
 
         <template v-slot:top>
           <v-toolbar flat>
             <v-toolbar-title>Expandable Table</v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-switch v-model="singleExpand" label="Single expand" class="mt-2"></v-switch>
+            <!-- <v-switch v-model="singleExpand" label="Single expand" class="mt-2"></v-switch> -->
+            <v-dialog v-model="dialog" max-width="500px">
+              <template v-slot:activator="{ on }">
+                <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">{{ formTitle }}</span>
+                </v-card-title>
+
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field v-model="editedItem.title" label="Title"></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="4">
+                        <v-text-field v-model="editedItem.completed" label="Completed"></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                  <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
           </v-toolbar>
         </template>
         <template v-slot:expanded-item="{ headers, item }">
-          <td :colspan="headers.length"><p>Title: {{ item.title }}</p> <p>ID: {{item._id}}</p>  </td>
+          <td :colspan="headers.length">
+            <p>Title: {{ item.title }}</p>
+            <p>ID: {{item._id}}</p>
+          </td>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+          <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
+        </template>
+        <template v-slot:no-data>
+          <v-btn color="primary" @click="initialize">Reset</v-btn>
         </template>
       </v-data-table>
     </v-card>
@@ -49,33 +88,54 @@ export default {
     getColor (completed) {
       if (completed) return 'red'
       else return 'green'
+    },
+    close () {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    save () {
+      if (this.editedIndex > -1) {
+        Object.assign(this.todos[this.editedIndex], this.editedItem)
+      } else {
+        this.todos.push(this.editedItem)
+      }
+      this.close()
     }
   },
   data () {
     return {
+      dialog: false,
+      editedIndex: -1,
+      editedItem: {
+        title: '',
+        completed: ''
+      },
+      defaultItem: {
+        title: '',
+        completed: false
+      },
       expanded: [],
       singleExpand: false,
       search: '',
-      todos: [
-        // {
-        //   title: 'a',
-        //   completed: false
-        // },
-        // {
-        //   title: 'b',
-        //   completed: false
-        // }
-      ],
+      todos: [],
       headers: [
         { text: 'Title', value: 'title' },
-        { text: 'Completed', value: 'completed' }
+        { text: 'Completed', value: 'completed' },
+        { text: 'Actions', value: 'actions', sortable: false }
       ]
     }
   },
   name: 'Home',
   components: {},
   computed: {
-    ...mapGetters(['allTodos'])
+    ...mapGetters(['allTodos']),
+    formTitle () {
+      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+    }
   },
   async created () {
     await this.fetchTodos()
