2a67a5c280bf05c2652e24a125b8073267db05c5
john morris <fennymorris@live.co.uk>
2020-07-29 23:12:25 +0100

    add components

--- tests/components/Greetings.spec.js
+++ tests/components/Greetings.spec.js
@@ -1,15 +1,29 @@
 /// <reference types="cypress" />
 import { mount } from 'cypress-vue-unit-test'
 import Greetings from '../../src/components/Greetings.vue'
+import AddTodo from '../../src/components/AddTodo.vue'
+import FilterTodo from '../../src/components/FilterTodo.vue'
+// import Todo from '../../src/components/Todo.vue'
 
-describe('Greetings', () => {
-  it('Works awesomely', () => {
-    // mount command comes from
-    // https://github.com/bahmutov/cypress-vue-unit-test
+describe('Mount', () => {
+  it('Greeting component', () => {
     mount(Greetings)
-
-    // now we can use any Cypress command to interact with the component
-    // https://on.cypress.io/api
     cy.get('[data-testid=title]').should('exist')
   })
+
+  it('AddTodo component', () => {
+    mount(AddTodo)
+    cy.get('h3').should('exist')
+    cy.get('[type="text"]').type('hello John')
+    cy.get('[type="submit"]').click()
+  })
+
+  it('FilterTodo component', () => {
+    mount(FilterTodo)
+    cy.get('select').select('3')
+  })
+
+  // it('Todo component', () => {
+  //   // mount(Todo)
+  // })
 })
